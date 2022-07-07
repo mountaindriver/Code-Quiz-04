babb20305f4f573c58f56a3bf505b8dd27dd2e0e
mountaindriver <lsfreigenberg@outlook.com>
2022-07-07 14:29:01 -0600

    second draft (the question work)

--- script.js
+++ script.js
@@ -10,67 +10,148 @@ var c = document.getElementById("c");
 var d = document.getElementById("d");
 var label = document.createElement("label");
 var input = document.createElement("input");
+var answerA = document.createElement("li");
+var answerB = document.createElement("li");
+var answerC = document.createElement("li");
+var answerD = document.createElement("li");
+var button = document.createElement("button");
 
 var score = 0;
 var time = 60;
 
 
 // START
-
 counter.textContent = `Time remaining: ${time}`;
 question.textContent = "Coding Quiz!";
 instructions.textContent = "Answer the following question! For every incorrect answer time will be removed from the clock! At the end of the quiz type your inital in to save your highscore!";
-a.textContent = "Start Quiz!";
-b.textContent = "";
-c.textContent = "";
-d.textContent = "";
+button.textContent = "PLAY GAME!"
+instructions.appendChild(button);
+
+
+button.addEventListener("click", function(){
+    question1();
+    instructions.appendChild(answerA);
+    instructions.appendChild(answerB);
+    instructions.appendChild(answerC);
+    instructions.appendChild(answerD);
+    instructions.removeChild(button);
+})
+
 
 // Why is this triggering?!
-document.getElementById("a").addEventListener("click", question1())
+// document.getElementById("a").addEventListener("click", question1())
 
 function question1(){
     timerStart();
     question.textContent = "What is not a JavaScript Data Type?"
-    a.textContent = "String"
-    b.textContent = "Boolean"
-    c.textContent = "Element"
-    d.textContent = "Undefined"
+    answerA.textContent = "String"
+    answerB.textContent = "Boolean"
+    answerC.textContent = "Element"
+    answerD.textContent = "Undefined"
     console.log("Question 1")
 
+    answerA.addEventListener("click", function(){
+        time - 10;
+        question2();
+    })
+    answerB.addEventListener("click", function(){
+        time - 10;
+        question2();
+    })
+    answerC.addEventListener("click", function(){
+        score + 10;
+        console.log("Correct")
+        question2();
+    })
+    answerD.addEventListener("click", function(){
+        time - 10;
+        question2();
+    })
     
 
 }
 
 function question2(){
     question.textContent = "What is the most used language in computer science?"
-    a.textContent = "HTML"
-    b.textContent = "Python"
-    c.textContent = "JavaScript"
-    d.textContent = "C#"
+    answerA.textContent = "HTML"
+    answerB.textContent = "Python"
+    answerC.textContent = "JavaScript"
+    answerD.textContent = "C#"
     console.log("Question 2")
 
+    answerA.addEventListener("click", function(){
+        time - 10;
+        question3();
+    })
+    answerB.addEventListener("click", function(){
+        time - 10;
+        question3();
+    })
+    answerC.addEventListener("click", function(){
+        score + 10;
+        console.log("Correct")
+        question3();
+    })
+    answerD.addEventListener("click", function(){
+        time - 10;
+        question3();
+    })
     
 }
 
 function question3(){
     question.textContent = "What is NOT a type of pop up boxes in JavaScript?"
-    a.textContent = "Alert"
-    b.textContent = "Confirm"
-    c.textContent = "Ad"
-    d.textContent = "Prompt"
+    answerA.textContent = "Alert"
+    answerB.textContent = "Confirm"
+    answerC.textContent = "Ad"
+    answerD.textContent = "Prompt"
     console.log("Question 3")
 
    
+    answerA.addEventListener("click", function(){
+        time - 10;
+        question4();
+    })
+    answerB.addEventListener("click", function(){
+        time - 10;
+        question4();
+    })
+    answerC.addEventListener("click", function(){
+        score + 10;
+        console.log("Correct")
+        question4();
+    })
+    answerD.addEventListener("click", function(){
+        time - 10;
+        question4();
+    })
 }
 
 function question4(){
     question.textContent = "What would be the result of 3+2+”7″?"
-    a.textContent = "12"
-    b.textContent = "35"
-    c.textContent = "57"
-    d.textContent = "327"
+    answerA.textContent = "12"
+    answerB.textContent = "35"
+    answerC.textContent = "57"
+    answerD.textContent = "327"
     console.log("Question 4")
 
+    answerA.addEventListener("click", function(){
+        time - 10;
+        gameOver();
+    })
+    answerB.addEventListener("click", function(){
+        time - 10;
+        gameOver();
+    })
+    answerC.addEventListener("click", function(){
+        score + 10;
+        console.log("Correct")
+        gameOver();
+    })
+    answerD.addEventListener("click", function(){
+        time - 10;
+        gameOver();
+    })
    
 }
 
@@ -79,7 +160,7 @@ function gameOver(){
     question.textContent = "All done!";
     instructions.textContent = "Your final Score is " + score;
     a.textContent = "Start";
-    label.textContent = "Initials:";
+
 
 }
 
@@ -89,11 +170,10 @@ function timerStart(){
         
         if (time = 0){
             alert ("YOU FAILED!")
-            return
-        }
-        if (highScores() == true){
             clearInterval(timeInterval)
+            score = 0;
             time = 60;
+            gameOver()
         }
         
     }, 1000);
